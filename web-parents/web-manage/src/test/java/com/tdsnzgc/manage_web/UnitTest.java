ff0e02471f94a74ebbf5492081aa5f7b9cd5e54a
123456+1s <akb48_1314520@163.com>
2021-03-07 16:54:51 +0800

    v0.6

--- web-parents/web-manage/src/test/java/com/tdsnzgc/manage_web/UnitTest.java
+++ web-parents/web-manage/src/test/java/com/tdsnzgc/manage_web/UnitTest.java
@@ -1,4 +1,18 @@
 package com.tdsnzgc.manage_web;
 
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.junit.Test;
+
 public class UnitTest {
+    @Test
+    public void jasyptTest() {
+//        BasicTextEncryptor encryptor = new BasicTextEncryptor();
+//        encryptor.setPassword("kxz1");
+//        String host = encryptor.decrypt("host");
+//        String username = encryptor.decrypt("username");
+//        String password = encryptor.decrypt("password");
+//        System.out.println(host);
+//        System.out.println(username);
+//        System.out.println(password);
+    }
 }
